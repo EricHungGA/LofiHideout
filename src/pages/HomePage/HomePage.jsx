6020775dd435e00ff8d6516c41da15d7ce87c942
EricHungGA <ehung303@gmail.com>
2023-04-17 20:59:10 -0700

    added tailwind navbar design and connected links

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,7 +5,9 @@ export default function HomePage({categories}) {
   return (
     <>
     <h1>HomePage</h1>
+    <div>
     {categories.map((c, idx) => <CategoryCard key={idx} category={c}/>)}
+    </div>
     </>
   )
 }
