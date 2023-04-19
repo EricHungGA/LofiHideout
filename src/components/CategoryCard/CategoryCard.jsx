978afd078e748d53919561b873847d348ffeb422
EricHungGA <ehung303@gmail.com>
2023-04-18 19:31:12 -0700

    updating the hover text styling on card

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -8,7 +8,8 @@ export default function CategoryCard({category}) {
   return (
 <>
     <div class="flex justify-center animate-hover-slow">
-    <div class="group relative block bg-black rounded-2xl h-128 w-72 border border-slate-400 transition overflow-hidden ring-8 ring-slate-600 ring-inset">
+    <div class="group relative block bg-black rounded-2xl h-128 w-72 border border-slate-400 transition overflow-hidden">
+    <div class="ring-8 ring-slate-600 ring-inset absolute inset-0 rounded-2xl"></div>
     <div class="p-8">
     <Link to={`/room/${category.name}`}>
       <div class="rounded-2xl absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-0 ">
@@ -34,7 +35,7 @@ export default function CategoryCard({category}) {
         <div class="mt-4 sm:mt-4 lg:mt-4">
         <div
           class="translate-y-8 transform opacity-0 transition-all duration-1000 group-hover:translate-y-0 group-hover:opacity-100">
-          <p class="text-xl text-white/60">
+          <p class="text-xl font-bold tracking-wider text-indigo-300/60">
             Enter Room
           </p>
         </div>
