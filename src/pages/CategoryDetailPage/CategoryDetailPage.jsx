824a8d7cca520e634c92549f071e7b5ec7f451be
EricHungGA <ehung303@gmail.com>
2023-04-19 11:45:29 -0700

    setting up background styling properties for the detail page

--- src/pages/CategoryDetailPage/CategoryDetailPage.jsx
+++ src/pages/CategoryDetailPage/CategoryDetailPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import './CategoryDetail.css'
 
 export default function CategoryDetailPage({categories}) {
     const { videoId } = useParams();
@@ -24,13 +25,18 @@ export default function CategoryDetailPage({categories}) {
       }
     
       return (
-        <div>
+        <>
+        <div class="background-1"></div>
+        <div class="background-2"></div>
+          <div class="bg-layer-1">
+          {category.ambienceType}<br></br>
           {category.name}
-          <iframe id="ytplayer" type="text/html" width="720" height="405"
+          <iframe class=" w-128 h-64 border-8 border-slate-500/20 border-inset opacity-80 shadow-lg shadow-green-500/50" id="ytplayer" type="text/html" width="720" height="405"
           src={`https://www.youtube.com/embed/${category.videoId}?&loop=1&controls=1`}
           frameBorder="0" allowFullScreen></iframe>
 
-          <div class="" id="player"></div>
+          <div id="player"></div>
         </div>
+        </>
       );
 }
