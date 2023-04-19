efafa072ad369749f03424889edf5fab4564ad14
EricHungGA <ehung303@gmail.com>
2023-04-18 17:03:41 -0700

    updating category card to link to the model properties and fixed seed data

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -14,23 +14,23 @@ export default function CategoryCard({category}) {
       <div class="rounded-2xl absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-0">
       <img
         alt="Developer"
-        src="https://i.pinimg.com/564x/3a/65/6d/3a656d679cdd9e5ad14ab231ef983abe.jpg"
+        src={category.cardImage}
         class="rounded-2xl absolute inset-0 object-cover h-full w-full transition-opacity"
       />
       </div>
       <div class="rounded-2xl absolute inset-0 h-full w-full object-cover opacity-0 transition-opacity group-hover:opacity-100">
       <img
         alt="Developer"
-        src="https://i.pinimg.com/564x/42/63/4f/42634f11d9c526fbee46543707500311.jpg"
+        src={category.hiddenCardImage}
         class="rounded-2xl absolute inset-0 object-cover h-full w-full"
       />
       </div>
       </Link>
       <div class="relative p-4 sm:p-6 lg:p-8 ">
         <p class="text-sm font-medium uppercase tracking-widest text-pink-500">
-          Rain
+          {category.ambienceType}
         </p>
-        <p class="text-xl font-bold text-white/70 sm:text-2xl">Lofi Hip-Hop</p>
+        <p class="text-xl font-bold text-white/70 sm:text-2xl">{category.name}</p>
         <div class="mt-4 sm:mt-4 lg:mt-4">
         <div
           class="translate-y-8 transform opacity-0 transition-all duration-1000 group-hover:translate-y-0 group-hover:opacity-100">
