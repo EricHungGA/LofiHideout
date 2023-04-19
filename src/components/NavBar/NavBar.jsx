3f72a2b0d9ba6ebc4aec91d7cdfef69acff762eb
EricHungGA <ehung303@gmail.com>
2023-04-19 01:23:40 -0700

    updating navbar to all links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -34,7 +34,7 @@ export default function NavBar({user, setUser}) {
           <img src="https://i.pinimg.com/564x/2a/3a/da/2a3adacf3ac01f5248ffd352dcece5a2.jpg"></img>
         </div>
         <div class="hidden sm:flex sm:items-center text-center">
-          <a href="/" class="text-orange-200/40 text-2xl italic tracking-wide hover:text-neutral-700/90">Lofi Hideout</a>
+          <Link to="/" class="text-orange-200/40 text-2xl italic tracking-wide hover:text-neutral-700/90">Lofi Hideout</Link>
         </div>
 
         <div class="hidden sm:flex sm:items-center">
@@ -44,12 +44,10 @@ export default function NavBar({user, setUser}) {
         }
         </div>
       </div>
-
       <div class="block sm:hidden bg-white border-t-2 py-2">
         <div class="flex flex-col">
           <a class="text-gray-800 text-sm font-semibold hover:text-purple-600 mb-1">Lofi Hideout</a>
           <div class="flex justify-between items-center border-t-2 pt-2">
-            <a href="#" class="text-gray-800 text-sm font-semibold border px-4 py-1 rounded-lg hover:text-blue-700 hover:border-blue-700">Sign up</a>
           </div>
         </div>
       </div>
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -9,6 +9,7 @@ export default function AuthPage({ setUser }) {
 
   return (
     <main className="AuthPage">
+      <style>{`body { background-image: url('https://lh3.googleusercontent.com/umvEZNvDSTaQJTigrtH0SSiyU_vI3uJmRLMLQkMHnHzh7Fb8NqiqRD8ykyXjL6821-Hzdao_6-1_6c5XHEqAtfUlu1IBKrTr83buTTb2jUWItMpRoxDMGWc3UKcrjYb3aS5E_I4cRswiTjEos0lLQrQCFkEh5iQkUfRJnRjtVaadgalkrhMhAcHLqyilclhntxVbrYrO1aeTg-4yFAHNd3wnI7DsatGywlhNjltdCFFjPi1JLhu1pAEWFuDOtj-eT2flTZUMgml71SSrzy3yDUpuH4ZwRSg6F4zXWVLXAAgDBguDY-BKteSuS8JSMbeP5M_kWda0Boj7kzSvyGD1c6vSFVpfeZGE2zxsIhVf7Jl803HezFczsHsB1xZdmC0I9-7rqAaxMTmIbxMv5F-4HvBJRcqQonTFgq4k5md0M2hXEPcQVwewCs5ksSGGb0BkSP2HAGd-xizoMeTLK-6-BY8AsnoJ7ZWIzYSyMhGMKq4mJu6aUj4QfB8Y6VcBG5-Wk--d-bjcrY0xFjO0wwL3w36prs06HItI8pp8iXG10GM-UVf19bGmzY_5O-cpeCEYy346mcmAFg5WllOy0GZBupuytQy1oSO7mapAJiTUdyHTvxVXK1xqXnCCK7B-ZFHVbLEgTt1KNOZYko87RadGyKUltfVzTNrEcDM7t1m3uxFdmOfHWGypr7MbjvSoSzWOT826_d78-777E1x5pp1vIWXVg5g4PIWcZqAcab2ASmUPFS8C8WfX1VdkXT-cqul25P0COQ8VaMuwarMU2lGat_3ZO1qkN-5xRvc7wa8_8pTEs8DMX-I4bSqiUGzA5T5tSYa8tAqFXpN0RpfCMx9j38WFec0VrmXKa4ysZ4VsX9Y0yc4ixji4sIA9biAVNzoTx--y83E6l6zOUUxus3DWDUaqm8qoOzrLEj98B4clmdO83ULPzkmKi0MMAChk795rNkQWey8cTD1H541MX7DNwNkWLlu217KJW8hZUNLf3RUsyofPXAUb-zrez5X1aJ8cWLt-mpcuYYDKTtmtGbPikHY=w560-h760-s-no?authuser=0'); }`}</style>
       {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser}/>}
       <div>
         <h3>{showLogin ? <button onClick={() => setShowLogin(!showLogin)}>SIGN UP</button>: <button onClick={() => setShowLogin(!showLogin)}>LOG IN</button>}</h3>
