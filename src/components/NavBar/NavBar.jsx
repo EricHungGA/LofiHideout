19826a3b579a2bd8915fb38209e842ad9bffb874
EricHungGA <ehung303@gmail.com>
2023-04-19 02:33:10 -0700

    simplified and centered navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -30,15 +30,19 @@ export default function NavBar({user, setUser}) {
 	<div class="bg-transparent animate-fade-in-and-out">
     <div class="container mx-auto px-4">
       <div class="flex items-center justify-between py-4">
-        <div class="w-10 h-10 opacity-70">
+        <div class="w-11 h-11 opacity-70">
           <img src="https://i.pinimg.com/564x/2a/3a/da/2a3adacf3ac01f5248ffd352dcece5a2.jpg"></img>
         </div>
         <div class="hidden sm:flex sm:items-center text-center">
-          <Link to="/" class="text-orange-200/40 text-2xl italic tracking-wide hover:text-neutral-700/90">Lofi Hideout</Link>
+          <Link to="/" class="text-orange-200/40 text-2xl italic tracking-wide hover:text-neutral-700/90 mb-2 ml-8">Lofi Hideout</Link>
         </div>
 
         <div class="hidden sm:flex sm:items-center">
-        {user ? <a onClick={handleLogOut} class="text-slate-400/90 text-sm font-semibold border px-4 py-2 rounded-lg hover:text-indigo-600/70 hover:border-indigo-600/70">Logout</a>
+        {user ?
+            <>
+            
+            <a onClick={handleLogOut} class="text-slate-400/90 text-sm font-semibold border px-4 py-2 rounded-lg hover:text-indigo-600/70 hover:border-indigo-600/70">Logout</a>
+            </>
             :
             <Link to="/api/users/signup" class="text-slate-400/90 text-sm font-semibold border px-4 py-2 rounded-lg hover:text-indigo-600/70 hover:border-indigo-600/70">Sign up</Link>
         }
